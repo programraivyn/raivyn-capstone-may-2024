f03ad5359666f4615f35acd220209840d238701c
Vita <theprototype@Vitas-MBP.lan>
2024-07-19 17:38:06 -0500

    finishing touches

---
+++ misc.js
@@ -0,0 +1,42 @@
+function desiresParty() {
+    let name;
+    let item;
+    name = prompt("What's your preferred name?");
+    suggestion = prompt("What location, activity or experience do you want to add for me?");
+    desiresHeader.innerHTML = `Thanks for contributing, ${name}!`;
+    desiresCart.innerHTML = `Your suggestion of ${suggestion} has been added to my Bucket List Desires! I'll be sure to tell you how it goes.`;
+  }
+  
+  button4Cart.onclick = desiresParty;
+
+  function experience1() {
+    let number, guess, guessNumber;
+    number = 17;
+    guess = prompt("Try to guess the number of states of I've been to so far..");
+    guessNumber = Number(guess);
+
+    if (guessNumber === number) {
+        shortPara.innerHTML = `Wow, yes! You guessed correctly. I've been to ${number} states!`;
+    } else {
+        shortPara.innerHTML = `Almost, perhaps? That's okay, give it another shot!`
+    }
+  }
+
+  experienceOne.onclick = experience1;
+
+  function toDoList() {
+    loopPara.innerHTML = "More things I'd like to scratch off the list..";
+
+    let activities = [`Building an App`, `Making Bread from Scratch`, `Launching a Website`, `Snorkeling`, `Getting a Tattoo`];
+    let highNumber = activities.length
+    for (let i = 0; i < highNumber; i++) {
+        loopList.innerHTML += `<li>${activities[i]}</li>`;
+    }
+  }
+oneOffActivities.onclick = toDoList; 
+
+document.getElementById("Bye4Now").innerHTML = untilNextTime("Dear friend");
+
+function untilNextTime(name) {
+    return alert(`You've made it to the JS party!`);
+}
